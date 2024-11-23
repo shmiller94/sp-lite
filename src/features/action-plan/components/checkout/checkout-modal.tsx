c82eae731d4b2a7b9c0a0a9491c5b69b42f2e8db
Nikita Mashchenko <52038455+nmashchenko@users.noreply.github.com>
2024-11-23 00:58:41 -0500

    feat: remove second step in checkout (#182)

--- src/features/action-plan/components/checkout/checkout-modal.tsx
+++ src/features/action-plan/components/checkout/checkout-modal.tsx
@@ -14,56 +14,20 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet';
 import { Body1, H2 } from '@/components/ui/typography';
-import {
-  CheckoutStoreProvider,
-  useCheckout,
-} from '@/features/action-plan/stores/checkout-store';
 import { useWindowDimensions } from '@/hooks/use-window-dimensions';
-import { StepItem, StepperStoreProvider, useStepper } from '@/lib/stepper';
 
-import { CheckoutStep } from './steps/checkout-step';
 import { ReviewStep } from './steps/review-step';
 
-export function ActionPlanCheckoutModal({ children }: { children: ReactNode }) {
-  const steps: StepItem[] = [
-    {
-      id: 'review',
-      content: <ReviewStep />,
-    },
-    {
-      id: 'checkout',
-      content: <CheckoutStep />,
-    },
-  ];
-
-  return (
-    <StepperStoreProvider steps={steps}>
-      <CheckoutStoreProvider>
-        <ActionPlanCheckoutModalConsumer>
-          {children}
-        </ActionPlanCheckoutModalConsumer>
-      </CheckoutStoreProvider>
-    </StepperStoreProvider>
-  );
-}
-
-function ActionPlanCheckoutModalConsumer({
+export const ActionPlanCheckoutModal = ({
   children,
 }: {
   children: ReactNode;
-}) {
+}) => {
   const { width } = useWindowDimensions();
-  const resetCheckout = useCheckout((s) => s.reset);
-  const resetSteps = useStepper((s) => s.resetSteps);
-
-  const reset = () => {
-    resetCheckout();
-    resetSteps();
-  };
 
   if (width <= 768) {
     return (
-      <Sheet onOpenChange={reset}>
+      <Sheet>
         <SheetTrigger asChild>{children}</SheetTrigger>
         <SheetContent className="flex max-h-full flex-col rounded-t-[10px]">
           <div className="flex items-center justify-between px-4 pt-16 md:pb-4">
@@ -76,14 +40,14 @@ function ActionPlanCheckoutModalConsumer({
             <div className="min-w-[44px]" />
           </div>
           <div className="overflow-auto">
-            <ActionPlanCheckoutContent />
+            <ReviewStep />
           </div>
         </SheetContent>
       </Sheet>
     );
   }
   return (
-    <Dialog onOpenChange={reset}>
+    <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent>
         <div className="flex flex-col">
@@ -100,17 +64,8 @@ function ActionPlanCheckoutModalConsumer({
             <H2>Checkout</H2>
           </div>
         </div>
-        <ActionPlanCheckoutContent />
+        <ReviewStep />
       </DialogContent>
     </Dialog>
   );
-}
-
-function ActionPlanCheckoutContent() {
-  const { steps, activeStep } = useStepper((s) => ({
-    steps: s.steps,
-    activeStep: s.activeStep,
-  }));
-
-  return <div className="w-full break-all">{steps[activeStep].content}</div>;
-}
+};
--- src/features/action-plan/components/checkout/steps/review-step.tsx
+++ src/features/action-plan/components/checkout/steps/review-step.tsx
@@ -1,113 +1,152 @@
-import { useState } from 'react';
+import React, { useState, useCallback, forwardRef } from 'react';
 
+import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
+import { DialogClose } from '@/components/ui/dialog';
 import { Body1 } from '@/components/ui/typography';
-import { useProducts } from '@/features/action-plan/api';
+import { useCreateCheckoutUrl, useProducts } from '@/features/action-plan/api';
 import { CheckoutPrice } from '@/features/action-plan/components/checkout/checkout-price';
-import { useCheckout } from '@/features/action-plan/stores/checkout-store';
 import { usePlan } from '@/features/action-plan/stores/plan-store';
 import { calculateTotals } from '@/features/action-plan/utils/calculate-totals';
-import { useStepper } from '@/lib/stepper';
 import { cn } from '@/lib/utils';
 import { Product } from '@/types/api';
 
 export const ReviewStep = (): JSX.Element => {
-  const { nextStep } = useStepper((s) => s);
   const productsQuery = useProducts();
+  const createCheckoutUrlMutation = useCreateCheckoutUrl({
+    mutationConfig: {
+      onSuccess: (data) => {
+        window.open(data.checkoutUrl, '_top', 'noreferrer');
+      },
+    },
+  });
   const goals = usePlan((s) => s.goals);
-  const { selectedProducts } = useCheckout((s) => s);
 
   const products = productsQuery.data?.products ?? [];
+  const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
 
   const totals = calculateTotals(selectedProducts);
+
+  const toggleProduct = useCallback((item: Product) => {
+    setSelectedProducts((prevSelected) => {
+      const isSelected = prevSelected.some((product) => product.id === item.id);
+      if (isSelected) {
+        return prevSelected.filter((product) => product.id !== item.id);
+      } else {
+        return [...prevSelected, item];
+      }
+    });
+  }, []);
 
   return (
     <>
       <div className="flex max-h-[50vh] flex-col gap-6 overflow-auto py-4">
-        {goals.map((goal, index) => {
+        {goals.map((goal, goalIndex) => {
           const productGoalItems = goal.goalItems.filter(
             (goalItem) => goalItem.itemType === 'PRODUCT',
           );
 
-          if (productGoalItems.length > 0) {
-            return (
-              <div className="flex flex-col gap-4" key={index}>
-                <Body1 className="px-10 text-zinc-500">
-                  {goal.title.length ? goal.title : `Goal ${index + 1}`}
-                </Body1>
-                <div className="flex flex-col gap-1 px-6">
-                  {productGoalItems.map((goalItem, index) => (
-                    <ActionPlanProductCheckoutOptionRow
-                      key={index}
-                      item={products.find(
-                        (product) => product.id === goalItem.itemId,
-                      )}
-                      initiallyChecked={
-                        !!selectedProducts?.find(
-                          (product) => product.id === goalItem.itemId,
-                        )
-                      }
-                    />
-                  ))}
-                </div>
-              </div>
-            );
-          }
+          if (productGoalItems.length === 0) return null;
 
-          return null;
+          return (
+            <div className="flex flex-col gap-4" key={goal.id || goalIndex}>
+              <Body1 className="px-10 text-zinc-500">
+                {goal.title.trim() ? goal.title : `Goal ${goalIndex + 1}`}
+              </Body1>
+              <div className="flex flex-col gap-1 px-6">
+                {productGoalItems.map((goalItem) => {
+                  const product = products.find(
+                    (p) => p.id === goalItem.itemId,
+                  );
+                  if (!product) return null;
+
+                  const isChecked = selectedProducts.some(
+                    (p) => p.id === product.id,
+                  );
+
+                  return (
+                    <ActionPlanProductCheckoutOptionRow
+                      key={product.id}
+                      item={product}
+                      onClick={toggleProduct}
+                      checked={isChecked}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          );
         })}
       </div>
-      <div className="flex w-full flex-col-reverse items-center gap-6 border-t border-t-zinc-200 p-5 md:w-auto md:flex-row md:justify-end">
-        <div className="flex flex-col items-center md:items-end">
-          <Body1 className="text-zinc-500">
-            Subtotal ({selectedProducts.length} items)
-          </Body1>
-          <Body1>${totals.total.toFixed(2)}</Body1>
+      <div
+        className={cn(
+          'flex w-full flex-col-reverse items-center p-5 border-t border-t-zinc-200 md:w-auto md:flex-row gap-2',
+          totals.totalSavings > 0 ? 'md:justify-between' : 'md:justify-end',
+        )}
+      >
+        {totals.totalSavings > 0 ? (
+          <Badge className="hidden bg-vermillion-50 px-3 py-2 md:inline-flex">
+            <Body1 className="text-vermillion-900">
+              Checkout and save ${totals.totalSavings.toFixed(2)}
+            </Body1>
+          </Badge>
+        ) : null}
+        <div className="flex w-full flex-col items-center gap-2 md:w-auto md:flex-row">
+          <div className="flex flex-col items-center md:items-end">
+            <Body1 className="text-zinc-500">
+              Subtotal ({selectedProducts.length}{' '}
+              {selectedProducts.length === 1 ? 'item' : 'items'})
+            </Body1>
+            <Body1>${totals.total.toFixed(2)}</Body1>
+          </div>
+          <DialogClose asChild>
+            <Button
+              onClick={() =>
+                createCheckoutUrlMutation.mutate({
+                  data: { products: selectedProducts },
+                })
+              }
+              disabled={selectedProducts.length === 0}
+              className="w-full md:w-auto"
+            >
+              Go to checkout
+            </Button>
+          </DialogClose>
         </div>
-        <Button
-          onClick={nextStep}
-          disabled={selectedProducts.length === 0}
-          className="w-full md:w-auto"
-        >
-          Go to checkout
-        </Button>
       </div>
     </>
   );
 };
 
-const ActionPlanProductCheckoutOptionRow = ({
-  item,
-  initiallyChecked,
-}: {
-  item?: Product;
-  initiallyChecked: boolean;
-}) => {
-  const [checked, setChecked] = useState(initiallyChecked);
-  const { updateSelectedProducts } = useCheckout((s) => s);
+interface ActionPlanProductCheckoutOptionRowProps {
+  item: Product;
+  onClick: (item: Product) => void;
+  checked: boolean;
+}
 
-  if (!item) return null;
-
-  const onItemClick = (): void => {
-    setChecked((prev) => !prev);
-
-    updateSelectedProducts(item);
+const ActionPlanProductCheckoutOptionRow = forwardRef<
+  HTMLDivElement,
+  ActionPlanProductCheckoutOptionRowProps
+>(({ item, onClick, checked }, ref) => {
+  const handleClick = () => {
+    onClick(item);
   };
 
   return (
     <div
+      ref={ref}
       className={cn(
         'flex cursor-pointer p-5 justify-between w-full items-center rounded-[20px] bg-zinc-100 gap-2',
       )}
       role="presentation"
-      onClick={onItemClick}
+      onClick={handleClick}
     >
       <div className="flex items-center gap-4">
         <img
           alt={item.name}
           src={item.image}
-          className="size-[64px] rounded-[12px] bg-white object-cover object-center"
+          className="size-16 rounded-[12px] bg-white object-cover object-center"
         />
 
         <div className="flex flex-col gap-1">
@@ -120,7 +159,11 @@ const ActionPlanProductCheckoutOptionRow = ({
       <Checkbox
         checked={checked}
         className="size-5 data-[state=unchecked]:border-zinc-200 data-[state=unchecked]:bg-white"
+        onChange={handleClick}
       />
     </div>
   );
-};
+});
+
+ActionPlanProductCheckoutOptionRow.displayName =
+  'ActionPlanProductCheckoutOptionRow';
