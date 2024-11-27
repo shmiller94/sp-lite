e8166275c4074a971d42d6bc20c4ba94e6923b4a
Fritz Stauffacher <fritzstauff@gmail.com>
2024-11-27 15:11:34 -0500

    Add more coupon info (#194)

--- src/features/onboarding/utils/gtm.ts
+++ src/features/onboarding/utils/gtm.ts
@@ -4,11 +4,15 @@ import { RewardfulWindow } from '@/types/window';
 export const trackSubscription = (price: SubscriptionPrice | undefined) => {
   try {
     const win = window as RewardfulWindow;
+    const accessCode = localStorage.getItem('superpower-code');
+
     if (win.dataLayer) {
       win.dataLayer.push({
         event: 'subscription',
         value: price?.total ?? 49900,
         referralId: win.Rewardful?.referral || null,
+        rewardfulCode: win.Rewardful?.coupon || null,
+        accessCode: accessCode || null,
       });
     }
   } catch (error) {
