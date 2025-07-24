dd74452dff712e75625b64da0229fc2c9f6c2549
Shawn Wáng /wɔŋ/ <Shawnxkwang@users.noreply.github.com>
2025-07-24 11:48:57 -0700

    Refactor prescriptions marketplace URLs for production and staging environments (#522)

--- src/app/routes/app/prescriptions-marketplace.tsx
+++ src/app/routes/app/prescriptions-marketplace.tsx
@@ -1,8 +1,16 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { PRESCRIPTIONS_MARKETPLACE_URL } from '@/const/marketplaces';
+import {
+  PRESCRIPTIONS_MARKETPLACE_URL_PROD,
+  PRESCRIPTIONS_MARKETPLACE_URL_STAGING,
+} from '@/const/marketplaces';
 import { useUser } from '@/lib/auth';
+
+const PRESCRIPTIONS_MARKETPLACE_URL =
+  process.env.NODE_ENV === 'production'
+    ? PRESCRIPTIONS_MARKETPLACE_URL_PROD
+    : PRESCRIPTIONS_MARKETPLACE_URL_STAGING;
 
 export const PrescriptionsMarketplaceRoute = () => {
   const navigate = useNavigate();
--- src/const/marketplaces.ts
+++ src/const/marketplaces.ts
@@ -1,2 +1,5 @@
 export const SUPPLEMENTS_MARKETPLACE_URL = 'https://products.superpower.com/';
-export const PRESCRIPTIONS_MARKETPLACE_URL = 'https://rx.superpower.com/';
+export const PRESCRIPTIONS_MARKETPLACE_URL_PROD =
+  'https://clinic.superpower.com/';
+export const PRESCRIPTIONS_MARKETPLACE_URL_STAGING =
+  'https://superpower-clinic.webflow.io';
