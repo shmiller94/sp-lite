691cca9d18abc39d2f44f6c67a3638336453c50b
Nikita Mashchenko <52038455+nmashchenko@users.noreply.github.com>
2025-08-15 02:31:50 -0500

    fix: hide advisory (#601)

--- src/features/services/components/services-list.tsx
+++ src/features/services/components/services-list.tsx
@@ -1,4 +1,5 @@
 import { Skeleton } from '@/components/ui/skeleton';
+import { ADVISORY_CALL } from '@/const';
 import { ENVIRONMENTAL_TOXIN_PANEL } from '@/const/toxin-panel';
 
 import { useServices } from '../api/get-services';
@@ -25,9 +26,13 @@ export const ServicesList = () => {
 
   if (!servicesQuery.data) return null;
 
-  const filteredServices = servicesQuery.data.services.filter(
-    (s) => !ENVIRONMENTAL_TOXIN_PANEL.find((panel) => panel.name === s.name),
-  );
+  const filteredServices = servicesQuery.data.services
+    .filter(
+      (s) => !ENVIRONMENTAL_TOXIN_PANEL.find((panel) => panel.name === s.name),
+    )
+    // hiding for now based on:
+    // https://linear.app/superpower/issue/ENG-3793/remove-advisory-call-service-on-frontend
+    .filter((s) => s.name !== ADVISORY_CALL);
 
   return (
     <div className="grid grid-cols-1 gap-1 sm:gap-x-3 sm:gap-y-9 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
