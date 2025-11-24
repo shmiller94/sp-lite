649b7f1fa2b718e40f65eac49e31b9ba426d1fe2
Florian Kiem <92092993+flornkm@users.noreply.github.com>
2025-11-24 12:09:44 +0100

    Florian/eng 10904 remove duplicate health records entry in settings (#1033)
    * chore: remove files from settings on desktop
    
    * chore: same on mobile

--- src/features/settings/components/settings-list-desktop.tsx
+++ src/features/settings/components/settings-list-desktop.tsx
@@ -8,7 +8,6 @@ import { Billing } from '@/features/settings/components/billing/billing';
 import { Membership } from '@/features/settings/components/membership/membership';
 import { Profile } from '@/features/settings/components/profile/profile';
 import { OrdersList } from '@/features/settings/components/purchases/orders-list';
-import { Vault } from '@/features/settings/components/vault/vault';
 import { WearablesTable } from '@/features/settings/components/wearables/wearables-table';
 import { capitalize } from '@/utils/format';
 
@@ -19,7 +18,6 @@ export const SettingsListDesktop = () => {
     { component: <Membership />, value: 'membership' },
     { component: <OrdersList />, value: 'history' },
     { component: <WearablesTable />, value: 'integrations' },
-    { component: <Vault />, value: 'health records' },
   ];
 
   return (
--- src/features/settings/components/settings-list-mobile.tsx
+++ src/features/settings/components/settings-list-mobile.tsx
@@ -6,7 +6,6 @@ import { Billing } from '@/features/settings/components/billing/billing';
 import { Membership } from '@/features/settings/components/membership/membership';
 import { Profile } from '@/features/settings/components/profile/profile';
 import { OrdersList } from '@/features/settings/components/purchases/orders-list';
-import { Vault } from '@/features/settings/components/vault/vault';
 import { WearablesTable } from '@/features/settings/components/wearables/wearables-table';
 import { SETTINGS_MOBILE } from '@/features/settings/const/settings-mobile';
 import { MobileMenu } from '@/features/settings/types/mobile-menu';
@@ -45,7 +44,6 @@ export const SettingsListMobile = () => {
         {current === 'membership' && <Membership />}
         {current === 'integrations' && <WearablesTable />}
         {current === 'order history' && <OrdersList />}
-        {current === 'health records' && <Vault />}
         {!current && (
           <>
             <div className="mb-2 flex h-[160px] gap-2">
